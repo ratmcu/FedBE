8ee74eeee0be38a35b06f3290ea08dd00a50689a
Rajitha Hathurusinghe <rhathuru@ford.com>
2024-11-03 12:18:23 -0500

    voting options

--- utils/options.py
+++ utils/options.py
@@ -48,6 +48,8 @@ def args_parser():
     parser.add_argument('--split_method', type=str, default='step', help='split_method, [step, dir]')
     
     parser.add_argument('--logit_method', type=str, default='vote', help='vote, else')
+    parser.add_argument('--is_logit', type=bool default=False, help='True, False')
+    parser.add_argument('--soft_vote', type=bool, default=False, help='True, False')
     
     # client regualrzation: FedProx
     parser.add_argument('--reg_type', type=str, default='', help='FedProx, scaffold')
